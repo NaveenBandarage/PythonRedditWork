2b52b80f564769d078867346b1372a463849a81d
Restyled.io <commits@restyled.io>
2021-10-05 11:20:20 +0000

    Restyled by autopep8

--- ReadingFromDatabase/readingFromDatabase.py
+++ ReadingFromDatabase/readingFromDatabase.py
@@ -7,11 +7,11 @@ import matplotlib.dates as mdates
 import pandas_datareader as pdr
 import sqlite3
 
-#python3 databaseName.db databaseTableName
+# python3 databaseName.db databaseTableName
 database = sys.argv[1]
 databaseTable = sys.argv[2]
 
-#Reading to the database  
+# Reading to the database
 conn = sqlite3.connect(database)
 
 cur = conn.cursor()
@@ -20,65 +20,66 @@ cur.execute("SELECT * FROM " + databaseTable)
 rows = cur.fetchall()
 
 for row in rows:
-  print(row)
+    print(row)
 
-#Converting to Dataframe 
-df = pd.DataFrame(rows, columns=['id','title','url','date','flair'])    
-print (df)
+# Converting to Dataframe
+df = pd.DataFrame(rows, columns=['id', 'title', 'url', 'date', 'flair'])
+print(df)
 
 conn.close()
 
-data={}
-ids=[]
-titles=[]
-urls=[]
-dates=[]
-flairs=[]
+data = {}
+ids = []
+titles = []
+urls = []
+dates = []
+flairs = []
 for stat in subStats:
     for items in subStats[stat]:
-      ids.append(items[0])
-      titles.append(items[1])
-      urls.append(items[2])
-      dates.append(items[5])
-      flairs.append(items[8])
+        ids.append(items[0])
+        titles.append(items[1])
+        urls.append(items[2])
+        dates.append(items[5])
+        flairs.append(items[8])
 
-data['id']=ids
-data['title']=titles
-data['url']=urls
-data['date']=dates
-data['flair']=flairs
-dataFrame=pd.DataFrame(data)
+data['id'] = ids
+data['title'] = titles
+data['url'] = urls
+data['date'] = dates
+data['flair'] = flairs
+dataFrame = pd.DataFrame(data)
 # dataFrame=dataFrame[dataFrame['flair']=='BTC']
-#need to look at this https://towardsdatascience.com/scraping-reddit-data-1c0af3040768 properly
+# need to look at this https://towardsdatascience.com/scraping-reddit-data-1c0af3040768 properly
 analyser = SentimentIntensityAnalyzer()
 
-scores=[]
+scores = []
 # print(data['title'])
 # print(data['date'])
 
 
 for title in data['title']:
-    sentiment_score=0
+    sentiment_score = 0
     try:
         for word in title:
-            sentiment_score=sentiment_score+analyser.polarity_scores(word)['compound']
+            sentiment_score = sentiment_score + \
+                analyser.polarity_scores(word)['compound']
     except TypeError:
         print("Error")
-        sentiment_score=0
-    
+        sentiment_score = 0
+
     scores.append(sentiment_score)
 print((scores))
 
-dataFrame['sentiment score']=scores
+dataFrame['sentiment score'] = scores
 
 
-dataFrame.index=dataFrame['date']
+dataFrame.index = dataFrame['date']
 # dataFrame=dataFrame.set_index('date', inplace=True)
-dataFrame=dataFrame.resample('D').mean()
+dataFrame = dataFrame.resample('D').mean()
 
-btc_data = pdr.get_data_yahoo(['BTC-USD'], 
-                          start=datetime.datetime(2020, 1, 1), 
-                          end=datetime.datetime(2020, 12, 29))['Close']
+btc_data = pdr.get_data_yahoo(['BTC-USD'],
+                              start=datetime.datetime(2020, 1, 1),
+                              end=datetime.datetime(2020, 12, 29))['Close']
 
 print(btc_data['BTC-USD'])
 x = dataFrame.index
@@ -86,11 +87,12 @@ y = dataFrame['sentiment score']
 print(x)
 print(y)
 
-plt.subplots(figsize=(30, 6)) #changing this helped with visualising 
+plt.subplots(figsize=(30, 6))  # changing this helped with visualising
 
 plt.plot(x, y)
-plt.xlabel("Date") #could pass these as parameters to help with the graphing not sure if this is the right approach. 
-plt.ylabel("Sentiment Score") #same with this as well.  
+# could pass these as parameters to help with the graphing not sure if this is the right approach.
+plt.xlabel("Date")
+plt.ylabel("Sentiment Score")  # same with this as well.
 # plt.set_xlabel('date')
 ax2 = plt.twinx()  # instantiate a second axes that shares the same x-axis
 ax2.set_ylabel('Price')
